14f88b7905d8ba18f2a39efe49af6b73652f1af2
Tanim0la <36541366+tanim0la@users.noreply.github.com>
2023-02-12 22:48:20 +0100

    Update Gou_staking.sol

--- contracts/Gou_staking.sol
+++ contracts/Gou_staking.sol
@@ -6,25 +6,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Gou_Staking is Ownable {
 
-    uint public totalStaked;
-    uint public apy;
+    uint256 public totalStaked;
+    uint256 public apy;
 
     address[] public stakers;
 
     mapping(address => bool) public isStaking;
-    mapping(address => uint) public stakedTime;
-    mapping(address => uint) public lastIssueTime;
-    mapping(address => uint) public rewards;
-    mapping(address => uint) public stakingBalance;
+    mapping(address => uint256) public stakedTime;
+    mapping(address => uint256) public lastIssueTime;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public stakingBalance;
 
     ERC20 public gou;
 
-    constructor(ERC20 _gou, uint _apy) {
+    constructor(ERC20 _gou, uint256 _apy) {
         gou = ERC20(_gou);
         apy = _apy * 1e18;
     }
 
-    function depositTokens(uint _amount) external {
+    function depositTokens(uint256 _amount) external {
         require(_amount > 99, 'AMOUNT LESS THAN 100');
         require(gou.balanceOf(_msgSender()) / 1e18 >= _amount, 'INSUFFICIENT BALANCE');
 
@@ -46,7 +46,7 @@ contract Gou_Staking is Ownable {
         require(stakingBalance[_msgSender()] >= 100, 'INSUFFICIENT BALANCE');
         require(isStaking[_msgSender()], 'YOU ARE NOT STAKING!!!');
 
-        uint bal = stakingBalance[_msgSender()];
+        uint256 bal = stakingBalance[_msgSender()];
         stakingBalance[_msgSender()] = 0;
         totalStaked -= bal;
         stakedTime[_msgSender()] = 0;
@@ -59,20 +59,20 @@ contract Gou_Staking is Ownable {
         require(rewards[_msgSender()] > 0, 'YOU DONT REWARDS TO CLAIM!');
         require((block.timestamp - stakedTime[_msgSender()]) >= 30 seconds, 'WAIT FOR 24 HOURS BEFORE CLAIM!!!');
 
-        uint claimR = rewards[_msgSender()];
+        uint256 claimR = rewards[_msgSender()];
         rewards[_msgSender()] = 0;
         gou.transfer(_msgSender(), claimR);
     }
 
     function issueRewards() external onlyOwner {
-        uint currentTime = block.timestamp;
-        for(uint i=0; i<stakers.length; i++){
+        uint256 currentTime = block.timestamp;
+        for(uint256 i=0; i<stakers.length; i++){
             address addy = stakers[i];
-            uint duration = currentTime - lastIssueTime[addy];
+            uint256 duration = currentTime - lastIssueTime[addy];
 
             if(duration >= 1 days) {
-                uint dRewards = ((apy / 100) * stakingBalance[addy]) / 365;
-                uint dayss = duration / 1 days;
+                uint256 dRewards = ((apy / 100) * stakingBalance[addy]) / 365;
+                uint256 dayss = duration / 1 days;
                 rewards[addy] += dRewards * dayss;
                 lastIssueTime[addy] = currentTime;
             }
